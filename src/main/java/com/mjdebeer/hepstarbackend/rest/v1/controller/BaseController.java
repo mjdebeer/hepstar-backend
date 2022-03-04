d504bdd2a9c2cc7e1b55472aa530916962adfe3e
MJ <webservices@mjdebeer.com>
2022-03-04 19:24:07 +0200

    Missed a spot.

--- src/main/java/com/mjdebeer/hepstarbackend/rest/v1/controller/BaseController.java
+++ src/main/java/com/mjdebeer/hepstarbackend/rest/v1/controller/BaseController.java
@@ -2,10 +2,18 @@ package com.mjdebeer.hepstarbackend.rest.v1.controller;
 
 import com.mjdebeer.hepstarbackend.configuration.SwaggerIncluded;
 import com.mjdebeer.hepstarbackend.rest.v1.response.BaseResponse;
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Api
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/test")
 public class BaseController {
 
     // service
