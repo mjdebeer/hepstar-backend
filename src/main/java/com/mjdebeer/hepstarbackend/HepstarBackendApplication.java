ee39ee7a4922d23a522aaf79584888a003624256
MJ <webservices@mjdebeer.com>
2022-03-04 18:03:41 +0200

    Initial commit.

---
+++ src/main/java/com/mjdebeer/hepstarbackend/HepstarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mjdebeer.hepstarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HepstarBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HepstarBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mjdebeer/hepstarbackend/HepstarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjdebeer.hepstarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HepstarBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
