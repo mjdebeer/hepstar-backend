5e12e0b7d13c437b6a0d52ad5e65c26632ab697d
MJ <webservices@mjdebeer.com>
2022-03-06 15:11:57 +0200

    Changed string conversion to Dom4j Document;

--- src/main/java/com/mjdebeer/hepstarbackend/rest/v1/controller/BaseController.java
+++ src/main/java/com/mjdebeer/hepstarbackend/rest/v1/controller/BaseController.java
@@ -1,25 +1,31 @@
 package com.mjdebeer.hepstarbackend.rest.v1.controller;
 
-import com.mjdebeer.hepstarbackend.services.request.RequestService;
 import com.mjdebeer.hepstarbackend.configuration.SwaggerIncluded;
 import com.mjdebeer.hepstarbackend.rest.v1.dto.ProductsPricedDto;
+import com.mjdebeer.hepstarbackend.services.request.RequestService;
 import com.mjdebeer.hepstarbackend.services.response.ResponseService;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.io.SAXReader;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import javax.xml.transform.stream.StreamSource;
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Stream;
 
 @Api
 @RestController
@@ -54,11 +60,9 @@ public class BaseController {
                 request,
                 String.class);
 
-        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-        InputStream inputStream = classloader.getResourceAsStream(response.getBody());
+        InputStream inputStream = new ByteArrayInputStream(response.getBody().getBytes(StandardCharsets.UTF_16));
         SAXReader reader = new SAXReader();
         Document productsResponse = reader.read(inputStream);
-
 
         return ResponseEntity.ok(responseService.readProductsDocumentAndGenerateResponse(productsResponse));
     }
